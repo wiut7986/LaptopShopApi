f651df4f854919e48409c7a3a12e2c670b0ced92
JasurbekDev <jasurbekdev@gmail.com>
2021-11-06 15:38:28 +0500

    added swagger

--- Startup.cs
+++ Startup.cs
@@ -45,9 +45,10 @@ namespace LaptopShopApi
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "LaptopShopApi v1"));
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "LaptopShopApi v1"));
 
             app.UseHttpsRedirection();
 
